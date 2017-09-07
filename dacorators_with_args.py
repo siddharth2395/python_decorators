8e19659e3c0e0b01b9abacb4a11850ba432c524c
siddharth bhavsar <siddharth2395@gmail.com>
2017-09-07 10:17:43 +0530

    dacorators with arguments

---
+++ dacorators_with_args.py
@@ -0,0 +1,16 @@
+def smart_divide(func):
+   def inner(a,b):
+      print("I am going to divide",a,"and",b)
+      if b == 0:
+         # print("Whoops! cannot divide")
+         return "Whoops! cannot divide"
+
+      return func(a,b)
+   return inner
+
+@smart_divide
+def divide(a,b):
+    return a/b
+
+print divide(2,3)
+print divide(2,0)
